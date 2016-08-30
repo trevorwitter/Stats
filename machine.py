c2c943052738907e944b81d94524e07d0913717f
Trevor Witter <trevorgwitter@gmail.com>
2016-08-29 20:45:11 -0400

    Update machine.py

--- machine.py
+++ machine.py
@@ -5,4 +5,9 @@ def split_data(data, prob):
     results[0 if random.random() < prob else 1]. append(row)
   return results 
   
-  
+def train_test_split(x, y, test_pct):
+    data = zip(x, y)          #pair corresponding values
+    train, test = split_data(data, 1 - test_pct)  #split the data set of pairs
+    x_train, y_train = zipi(*train)       #unzip
+    x_test, y_test = zip(*test)
+    return x_train, x_test, y_train, y_test
